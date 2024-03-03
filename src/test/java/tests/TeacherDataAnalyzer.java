4730246f0d21a2cc262ce6348e3db1b904e9d0cc
vund92 <vund30992@gmail.com>
2024-03-03 18:03:15 +0700

    CSV Utils code

--- src/test/java/tests/TeacherDataAnalyzer.java
+++ src/test/java/tests/TeacherDataAnalyzer.java
@@ -47,47 +47,47 @@ public class TeacherDataAnalyzer {
         }
     }
 
-    public static List<TeacherPojo> readTeacherDataFromCSVFile(String csvFile){
-        List<TeacherPojo> teacherPojoList = new ArrayList<>();
-
-        try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
-            // Read CSV data
-            List<String[]> csvData = reader.readAll();
-
-            csvData.remove(0); //remove the headers
-
-            // Iterate through each row and create tests.TeacherPojo objects
-            for (String[] row : csvData) {
-                String id = row[0];
-                String teacherName = row[1];
-                String teacherMinute = row[2];
-
-                TeacherPojo teacherPojo = new TeacherPojo(id, teacherName, teacherMinute);
-
-                // Iterate over members and add to the list
-                for (int i = 3; i < row.length; i += 2) {
-                    if (row[i] != null && row[i + 1] != null) {
-                        String memberName = row[i];
-                        String memberMinute = row[i + 1];
-
-                        MemberPojo member = new MemberPojo(memberName, memberMinute);
-                        teacherPojo.getMembers().add(member);
-                    }
-                }
-
-                teacherPojoList.add(teacherPojo);
-            }
-
-//            // Print the data
-//            for (TeacherPojo teacherData : teacherPojoList) {
-//                System.out.println(teacherData);
+//    public static List<TeacherPojo> readTeacherDataFromCSVFile(String csvFile){
+//        List<TeacherPojo> teacherPojoList = new ArrayList<>();
+//
+//        try (CSVReader reader = new CSVReader(new FileReader(csvFile))) {
+//            // Read CSV data
+//            List<String[]> csvData = reader.readAll();
+//
+//            csvData.remove(0); //remove the headers
+//
+//            // Iterate through each row and create tests.TeacherPojo objects
+//            for (String[] row : csvData) {
+//                String id = row[0];
+//                String teacherName = row[1];
+//                String teacherMinute = row[2];
+//
+//                TeacherPojo teacherPojo = new TeacherPojo(id, teacherName, teacherMinute);
+//
+//                // Iterate over members and add to the list
+//                for (int i = 3; i < row.length; i += 2) {
+//                    if (row[i] != null && row[i + 1] != null) {
+//                        String memberName = row[i];
+//                        String memberMinute = row[i + 1];
+//
+//                        MemberPojo member = new MemberPojo(memberName, memberMinute);
+//                        teacherPojo.getMembers().add(member);
+//                    }
+//                }
+//
+//                teacherPojoList.add(teacherPojo);
 //            }
-        } catch (IOException | CsvException e) {
-            e.printStackTrace();
-        }
-
-        return teacherPojoList;
-    }
+//
+////            // Print the data
+////            for (TeacherPojo teacherData : teacherPojoList) {
+////                System.out.println(teacherData);
+////            }
+//        } catch (IOException | CsvException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return teacherPojoList;
+//    }
 
     public static List<TeacherPojo> getQualifiedClasses(List<TeacherPojo> teacherPojoList){
         List<TeacherPojo> qualifiedClasses = teacherPojoList.stream()
--- src/test/java/tests/Tests.java
+++ src/test/java/tests/Tests.java
@@ -5,17 +5,21 @@ import org.testng.annotations.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static tests.CSVUtils.readTeacherDataFromCSVFile;
+import static tests.TeacherDataAnalyzer.getQualifiedClasses;
+import static tests.TeacherDataAnalyzer.getUnqualifiedClasses;
+
 public class Tests {
     @Test
     public static void printClassCountsForEachTeacherName() {
         String csvFile = "src/main/resources/MOCK_DATA.csv";
         List<TeacherPojo> teacherPojoList = new ArrayList<>();
-        teacherPojoList = TeacherDataAnalyzer.readTeacherDataFromCSVFile(csvFile);
+        teacherPojoList = readTeacherDataFromCSVFile(csvFile);
         List<TeacherPojo> qualitifiedClasses = new ArrayList<>();
-        qualitifiedClasses = TeacherDataAnalyzer.getQualifiedClasses(teacherPojoList);
+        qualitifiedClasses = getQualifiedClasses(teacherPojoList);
         System.out.println("Qualitified Class Count = " + qualitifiedClasses.size());
         List<TeacherPojo> unqualitifiedClasses = new ArrayList<>();
-        unqualitifiedClasses = TeacherDataAnalyzer.getUnqualifiedClasses(teacherPojoList,qualitifiedClasses);
+        unqualitifiedClasses = getUnqualifiedClasses(teacherPojoList,qualitifiedClasses);
         System.out.println("Unqualitified Class Count = " + unqualitifiedClasses.size());
         System.out.println("-----");
         TeacherDataAnalyzer.printClassCountsAndSalary(qualitifiedClasses, unqualitifiedClasses);
